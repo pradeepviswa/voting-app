44c5e9007b3677bbc8be73ed4df8c9f9894df422
Pradeep Viswanathan <74972223+pradeepviswa@users.noreply.github.com>
2026-03-16 23:27:12 +0530

    Update vote.php

--- voting/vote.php
+++ voting/vote.php
@@ -1,1 +1,16 @@
 
+<?php
+include "db.php";
+
+if(isset($_POST['party'])){
+
+$party = $_POST['party'];
+
+$sql = "INSERT INTO votes (party) VALUES ('$party')";
+$conn->query($sql);
+
+echo "<h2>Thank you for voting!</h2>";
+echo "<a href='results.php'>See Results</a>";
+}
+
+?>
