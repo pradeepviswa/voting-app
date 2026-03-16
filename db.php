f761c4b5aeefeeadcf90d6055dc30b49922f8ebd
Pradeep Viswanathan <74972223+pradeepviswa@users.noreply.github.com>
2026-03-17 00:36:41 +0530

    Update db.php

--- db.php
+++ db.php
@@ -1,13 +1,14 @@
-
 <?php
-$host = "localhost";
+
+$host = "voting-mysql";
 $user = "root";
-$password = "";
+$password = "root";
 $database = "voting_db";
 
-$conn = new mysqli($host, $user, $password, $database);
+$conn = new mysqli($host,$user,$password,$database);
 
 if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
+ die("Connection failed: " . $conn->connect_error);
 }
+
 ?>
